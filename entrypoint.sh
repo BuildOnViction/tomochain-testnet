024c678fac634a4b5294ba459eba641c64116bd2
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2018-05-15 04:26:56 +0000

    change code-based to tomo

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,24 +1,11 @@
 #!/bin/bash
 
-input="/build/.bootnodes"
-bootnodes=""
-while IFS= read -r line
-do
-  if [ -z "${bootnodes}" ]
-  then
-    bootnodes=$line
-  else
-    bootnodes="${bootnodes},$line"
-  fi
-done < "$input"
-echo $bootnodes
-
-if [ ! -d /build/vet/geth/chaindata ]
+if [ ! -d /build/tomochain/tomo/chaindata ]
 then
-  wallet=$(geth account new --password /build/.pwd --datadir /build/vet | awk -v FS="({|})" '{print $2}')
-  geth --datadir /build/vet init /build/genesis.json
-  geth --bootnodes ${bootnodes} --datadir /build/vet --networkid 40686 --rpc --rpccorsdomain "*" --rpcaddr 0.0.0.0 --rpcport 8545 --unlock "${wallet}" --password /build/.pwd --mine --gasprice "100000000" --targetgaslimit "42000000"
+  wallet=$(tomo account new --password /build/.pwd --datadir /build/tomochain | awk -v FS="({|})" '{print $2}')
+  sed "s/:wallet:/${wallet}/g" /build/tomochain.json > /build/genesis.json
+  tomo --datadir /build/tomochain init /build/genesis.json
 else
-  wallet=$(geth account list --datadir /build/vet| head -n 1 | awk -v FS="({|})" '{print $2}')
-  geth --bootnodes ${bootnodes} --datadir /build/vet --networkid 40686 --rpc --rpccorsdomain "*" --rpcaddr 0.0.0.0 --rpcport 8545 --unlock "${wallet}" --password /build/.pwd --mine --gasprice "100000000" --targetgaslimit "42000000"
+  wallet=$(tomo account list --datadir /build/tomochain| head -n 1 | awk -v FS="({|})" '{print $2}')
 fi
+tomo --datadir /build/tomochain --networkid 89 --rpc --rpccorsdomain "*" --rpcaddr 0.0.0.0 --rpcport 8545 --unlock "${wallet}" --password /build/.pwd --mine --gasprice "100000000" --targetgaslimit "42000000"
