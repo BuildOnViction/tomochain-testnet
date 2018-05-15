70c0da6049a699f645c37bdba7ef06b9a12075dd
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2018-05-15 05:23:22 +0000

    support init first  node

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,32 @@
 if [ ! -d /build/tomochain/tomo/chaindata ]
 then
   wallet=$(tomo account new --password /build/.pwd --datadir /build/tomochain | awk -v FS="({|})" '{print $2}')
-  sed "s/:wallet:/${wallet}/g" /build/tomochain.json > /build/genesis.json
+  if [[ -v FIRST_NODE ]]
+  then
+      sed "s/:wallet:/${wallet}/g" /build/tomochain.json > /build/genesis.json
+  fi
   tomo --datadir /build/tomochain init /build/genesis.json
 else
   wallet=$(tomo account list --datadir /build/tomochain| head -n 1 | awk -v FS="({|})" '{print $2}')
 fi
-tomo --datadir /build/tomochain --networkid 89 --rpc --rpccorsdomain "*" --rpcaddr 0.0.0.0 --rpcport 8545 --unlock "${wallet}" --password /build/.pwd --mine --gasprice "100000000" --targetgaslimit "42000000"
+
+input="/build/.bootnodes"
+bootnodes=""
+while IFS= read -r line
+do
+    if [ -z "${bootnodes}" ]
+    then
+        bootnodes=$line
+    else
+        bootnodes="${bootnodes},$line"
+    fi
+done < "$input"
+
+echo FIRST_NODE is ${FIRST_NODE}
+if [[ -v FIRST_NODE ]]; then
+    echo Starting the first node ...
+    tomo --datadir /build/tomochain --networkid 89 --rpc --rpccorsdomain "*" --rpcaddr 0.0.0.0 --rpcport 8545 --unlock "${wallet}" --password /build/.pwd --mine --gasprice "1" --targetgaslimit "420000000"
+else
+    echo Starting nodes with bootnodes $bootnodes ...
+    tomo --bootnodes ${bootnodes}  --datadir /build/tomochain --networkid 89 --rpc --rpccorsdomain "*" --rpcaddr 0.0.0.0 --rpcport 8545 --unlock "${wallet}" --password /build/.pwd --mine --gasprice "1" --targetgaslimit "420000000"
+fi
